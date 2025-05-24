45312aa0f2458a7de444b44db946dca023c4f191
rahulakula07 <rahulakula4265@gmail.com>
2025-05-24 13:17:26 +0530

    added with credentials true

--- Backend/server.js
+++ Backend/server.js
@@ -19,7 +19,7 @@ const PORT=process.env.PORT ||5000;
 // const __dirname = path.resolve()
 
 app.use(cors({
-    origin:["http://localhost:5173", "https://mer-nproject-liart.vercel.app"]
+    origin:["http://localhost:5173", "https://mer-nproject-liart.vercel.app"], credentials:true
 }))
 
 app.use(express.json({limit:"10mb"}))
